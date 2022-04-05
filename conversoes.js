d3456b2333234ab38a6a7f4783b8d637be60e169
Elvin <elvin-mig@hotmail.com>
2022-04-05 19:59:25 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+//tipo de dados
+//booleanos
+
+//conversão implícita 
+const numero = 456; 
+const numeroString = "456"
+
+console.log(numero == numeroString)
+
+//conversao explícita
+
+console.log(Number(numeroString) + numero)
