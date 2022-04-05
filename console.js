ad7b2f209455f3bce06b06772cafe2d8cf9e53fd
Elvin <elvin-mig@hotmail.com>
2022-04-05 20:55:12 -0300

    console e erros

---
+++ console.js
@@ -0,0 +1,6 @@
+//log --> registro
+console.log();
+
+//tratamento de erro!
+console.error('Deu erro!');
+console.warn();
---
+++ erros.js
@@ -0,0 +1,4 @@
+const numero = 0; 
+
+console.log(minhavar);//ReferenceError: minhavar is not defined
+
