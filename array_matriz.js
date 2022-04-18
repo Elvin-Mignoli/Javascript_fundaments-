46feb6cbd7bd0fef07b4ecfd42d75ad35df4e9b3
Elvin <elvin-mig@hotmail.com>
2022-04-18 20:47:55 -0300

    Aula de array

---
+++ array_matriz.js
@@ -0,0 +1,12 @@
+
+            //     0       1        2      3
+const alunos = ['João','Juliana', 'Caio','Ana'];
+
+                        //0,1,2,3
+const mediasDosAlunos = [10,7,9,6];
+
+                          //0[alunos] , 1[mediasDosAlunos]
+let listaDeNotasEAlunos = [alunos,     mediasDosAlunos];
+
+console.log(`${listaDeNotasEAlunos[0][0]}, sua média é ${listaDeNotasEAlunos[1][0]}`);
+
