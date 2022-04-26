3bb5fda8b36b56277d8e7d9ddabf46c439bfa26c
Elvin <elvin-mig@hotmail.com>
2022-04-26 20:32:53 -0300

    Aulas de orndenação

---
+++ Algoritmos/alg_first.js
@@ -0,0 +1,16 @@
+const livros = require('./alg_list');
+
+function menorValor(arrProdutos, posicaoInicial){
+ 
+    let maisBarato = posicaoInicial;
+
+    for(let atual = posicaoInicial; atual < livros.length; atual++){
+        //código
+        if(arrProdutos[atual].preco  < arrProdutos[maisBarato].preco){
+            maisBarato = atual;
+        }
+    }
+    return maisBarato
+}
+
+module.exports = menorValor;
